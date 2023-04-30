482ba9960cdc970175d7a323c2923675e1c8e9a2
james <smr01234585@gmail.com>
2023-04-30 21:34:31 +0500

    add docker and fastapi

--- web/app/Http/Controllers/ModelController.php
+++ web/app/Http/Controllers/ModelController.php
@@ -10,4 +10,16 @@ class ModelController extends Controller
     {
         return response()->json();
     }
+
+    public function predict()
+    {
+        $command = escapeshellcmd('././model/main.py/');
+        $output = shell_exec($command);
+        return response()->json([
+            'predict' => $output
+        ]);
+        return redirect()->back()->with([
+            'prediction' => $output
+        ]);
+    }
 }
--- web/routes/web.php
+++ web/routes/web.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ModelController::class, 'index']);
+Route::get('/predict', [ModelController::class, 'predict']);
